9ebc0accd707ba0d5cb1829e7242aff100a0292b
eddybruv <edwinajong@gmail.com>
2022-06-19 21:31:00 +0100

    sandbox

---
+++ sandbox.ts
@@ -0,0 +1,20 @@
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// arrays
+let ninjas: string[] = [];
+
+// union types
+let mixed: (string | number)[] = [];
+let uid: string | number;
+
+// objects
+let ninjaOne: object;
+
+let ninjaTwo: {
+  name: string;
+  age: number;
+  beltColor: string;
+};
