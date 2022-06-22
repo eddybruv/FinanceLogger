e8467603ca36022f4c220f9c3e685b26f27a8fd8
eddybruv <edwinajong@gmail.com>
2022-06-22 13:49:28 +0100

    wrapped up project

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,62 @@
-import Invoice from "./classes/invoice";
-
-const invOne = new Invoice('mario', "work on the mario website", 400);
-const invTwo = new Invoice('luigi', "work on the luigi website", 500)
-
-let invoices: Invoice[] = [];
-
-invoices.push(invTwo);
-invoices.push(invOne);
-
-invoices.forEach(inv => {
-    console.log(inv.format());
-})
+import Invoice from "./classes/invoice.js";
+import Payment from "./classes/Payment.js";
+import {HasFormatter} from "./interfaces/HasFormatter.js"
+import {ListTemplate} from "./classes/ListTemplate.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
-const type = document.querySelector('#type') as HTMLSelectElement;
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form.addEventListener("submit", (e: Event) => {
-    e.preventDefault();
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    )
-})
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  let values: [string, string, number] = [tofrom.value, details.value, amount.valueAsNumber];
+
+  let doc: HasFormatter;
+
+  if(type.value === "invoice") {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+
+  list.render(doc, type.value, "end");
+});
+
+
+// ENUMS
+enum ResourceType {BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
+
+// generics
+const addUID = <T extends {name: string}> (obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return {...obj, uid};
+}
+
+let docOne = addUID({name: "Edwin", age: 21});
+console.log(docOne)
+
+interface Resource<T> {
+  uid: number;
+  resourceName: ResourceType;
+  data: T;
+}
+
+const docThree : Resource<string> = {
+  uid: 1,
+  resourceName: ResourceType.PERSON,
+  data: "Eddy"
+}
+
+// tuples
+let arr = ["ryu", 25, true];
+
+let tup: [string, number, boolean] = ["ryu", 25, true];
