bcd4f3acb2d695ab030ac66838b46d40b867d0db
eddybruv <edwinajong@gmail.com>
2022-06-22 13:49:11 +0100

    generated code from ts files

---
+++ public/app.js
@@ -0,0 +1,48 @@
+import Invoice from "./classes/invoice.js";
+import Payment from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+const form = document.querySelector(".new-item-form");
+//inputs
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let values = [tofrom.value, details.value, amount.valueAsNumber];
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, "end");
+});
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+// generics
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: "Edwin", age: 21 });
+console.log(docOne);
+const docThree = {
+    uid: 1,
+    resourceName: ResourceType.PERSON,
+    data: "Eddy"
+};
+// tuples
+let arr = ["ryu", 25, true];
+let tup = ["ryu", 25, true];
